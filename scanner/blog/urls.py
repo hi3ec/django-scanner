049f54f36c1a5b29565de1acb278d3742b647243
sh <angelman999@protonmail.com>
2020-10-10 17:40:54 +0330

    :q

--- scanner/blog/urls.py
+++ scanner/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, dashboard, login, tables
 
 app_name = "blog"
 urlpatterns = [
     path('', home, name="home"),
+    path('dashboard/', dashboard, name="dashboard"),
+    path('login/', login, name="login"),
+    path('tables/', tables, name="tables"),
 ]
--- scanner/blog/views.py
+++ scanner/blog/views.py
@@ -7,4 +7,19 @@ def home(request):
     context = {
         "Devices": Devices.objects.all()
     }
+    return render(request, 'blog/login.html', context)
+
+def dashboard(request):
+    context = {
+        "Devices": Devices.objects.all()
+    }
     return render(request, 'blog/index.html', context)
+
+def login(request):
+    return render(request, 'blog/login.html')
+
+def tables(request):
+    context = {
+            "Devices": Devices.objects.all()
+        }
+    return render(request, 'blog/tables.html', context)
